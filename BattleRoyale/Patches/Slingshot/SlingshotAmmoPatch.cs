be250afe92fdd119c3f0d7929f5a68f0a75d272d
Tyler <gibbstyler7@gmail.com>
2021-12-08 13:59:27 -0600

    clean up slingshot patch

--- BattleRoyale/Patches/Slingshot/SlingshotAmmoPatch.cs
+++ BattleRoyale/Patches/Slingshot/SlingshotAmmoPatch.cs
@@ -8,25 +8,23 @@ namespace BattleRoyale.Patches
     {
         protected override PatchDescriptor GetPatchDescriptor() => new(typeof(Slingshot), "DoFunction");
 
-        private static StardewValley.Object oldObject;
+        private static Object oldObject;
         private static int oldStack = 0;
         private static int oldProjectilesCount = 0;
 
-        public static void Prefix(Slingshot __instance, GameLocation location, int x, int y, int power, Farmer who)
+        public static void Prefix(Slingshot __instance, GameLocation location)
         {
             oldObject = __instance.attachments[0];
             oldStack = oldObject?.Stack ?? 0;
             oldProjectilesCount = location.projectiles?.Count ?? 0;
         }
 
-        public static void Postfix(Slingshot __instance, GameLocation location, int x, int y, int power, Farmer who)
+        public static void Postfix(Slingshot __instance, GameLocation location)
         {
             if ((location.projectiles?.Count ?? 0) == oldProjectilesCount)
             {
                 if (__instance.attachments[0] == null)
-                {
                     __instance.attachments[0] = oldObject;
-                }
 
                 if (oldObject != null)
                     oldObject.Stack = oldStack;
