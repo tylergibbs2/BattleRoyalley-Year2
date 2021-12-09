919239b1286d93f747c1fd9d2853db5f546de0b4
Tyler <gibbstyler7@gmail.com>
2021-12-08 23:49:29 -0600

    stops creating a new random every list shuffle

--- BattleRoyale/Extensions/ListShuffleExtension.cs
+++ BattleRoyale/Extensions/ListShuffleExtension.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using StardewValley;
 using System.Collections.Generic;
 
 namespace BattleRoyale
@@ -7,13 +7,11 @@ namespace BattleRoyale
     {
         public static void Shuffle<T>(this IList<T> list)
         {
-            Random random = new();
-
             int n = list.Count;
             while (n > 1)
             {
                 n--;
-                int k = random.Next(n + 1);
+                int k = Game1.random.Next(n + 1);
                 T value = list[k];
                 list[k] = list[n];
                 list[n] = value;
